41ea76dd0cff92ac4212d2e7f3a314602f68d631
Hanzei <hanzei@mailbox.org>
2019-06-03 10:08:46 +0200

    Use bot account & remove dead code

--- server/http_hooks.go
+++ server/http_hooks.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"html/template"
 	"net/http"
 
 	"github.com/mattermost/mattermost-server/model"
@@ -14,10 +12,8 @@ import (
 // /plugins/{id} path will be routed to the plugin.
 func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
 	var action *Action
-	json.NewDecoder(r.Body).Decode(&action)
-
-	if action == nil {
-		encodeEphermalMessage(w, "WelcomeBot Error: We could not decode the action")
+	if err := json.NewDecoder(r.Body).Decode(&action); err != nil || action == nil {
+		p.encodeEphermalMessage(w, "WelcomeBot Error: We could not decode the action")
 		return
 	}
 
@@ -26,19 +22,19 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 
 	if data.User, err = p.API.GetUser(action.Context.UserID); err != nil {
 		p.API.LogError("failed to query user", "user_id", action.Context.UserID)
-		encodeEphermalMessage(w, "WelcomeBot Error: We could not find the supplied user")
+		p.encodeEphermalMessage(w, "WelcomeBot Error: We could not find the supplied user")
 		return
 	}
 
 	if data.Team, err = p.API.GetTeam(action.Context.TeamID); err != nil {
 		p.API.LogError("failed to query team", "team_id", action.Context.TeamID)
-		encodeEphermalMessage(w, "WelcomeBot Error: We could not find the supplied team")
+		p.encodeEphermalMessage(w, "WelcomeBot Error: We could not find the supplied team")
 		return
 	}
 
-	if data.DirectMessage, err = p.API.GetDirectChannel(action.Context.UserID, p.welcomeBotUserID); err != nil {
+	if data.DirectMessage, err = p.API.GetDirectChannel(action.Context.UserID, p.botUserID); err != nil {
 		p.API.LogError("failed to query direct message channel", "user_id", action.Context.UserID)
-		encodeEphermalMessage(w, "WelcomeBot Error: We could not find the welcome bot direct message channel")
+		p.encodeEphermalMessage(w, "WelcomeBot Error: We could not find the welcome bot direct message channel")
 		return
 	}
 
@@ -47,127 +43,38 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 	// Check to make sure you're still in the team
 	if teamMember, err := p.API.GetTeamMember(action.Context.TeamID, action.Context.UserID); err != nil || teamMember == nil || teamMember.DeleteAt > 0 {
 		p.API.LogError("Didn't have access to team", "user_id", action.Context.UserID, "team_id", action.Context.TeamID)
-		encodeEphermalMessage(w, "WelcomeBot Error: You do not appear to have access to this team")
+		p.encodeEphermalMessage(w, "WelcomeBot Error: You do not appear to have access to this team")
 		return
 	}
 
 	switch r.URL.Path {
 	case "/addchannels":
-
 		for _, wm := range p.getWelcomeMessages() {
-
 			if data.Team.Name == wm.TeamName {
 				for _, ac := range wm.Actions {
 					if ac.ActionName == action.Context.Action {
 						p.processActionMessage(*data, action, *ac)
-						encodeEphermalMessage(w, "")
+						p.encodeEphermalMessage(w, "")
 						return
 					}
 				}
 			}
 		}
 
-		encodeEphermalMessage(w, "WelcomeBot Error: The action wasn't found for "+action.Context.Action)
+		p.encodeEphermalMessage(w, "WelcomeBot Error: The action wasn't found for "+action.Context.Action)
 	default:
 		http.NotFound(w, r)
 	}
 }
 
-func (p *Plugin) handleSupport(action *Action, data *MessageTemplate) {
-	p.joinChannel(action, "peer-to-peer-help")
-	p.joinChannel(action, "bugs")
-
-	template, _ := template.New("Response").Parse(
-		`### Awesome, I have added you to the following support related channels!
-~peer-to-peer-help - General help and setup
-~bugs - To help investigate or report bugs
-`)
-	var message bytes.Buffer
-	template.Execute(&message, data)
-
-	post := &model.Post{
-		Message:   message.String(),
-		ChannelId: data.DirectMessage.Id,
-		UserId:    p.welcomeBotUserID,
-	}
-
-	if _, err := p.API.CreatePost(post); err != nil {
-		p.API.LogError(
-			"We could not create the response post",
-			"user_id", post.UserId,
-			"err", err.Error(),
-		)
-	}
-}
-
-func (p *Plugin) handelDeveloper(action *Action, data *MessageTemplate) {
-	p.joinChannel(action, "developers")
-	p.joinChannel(action, "developer-toolkit")
-	p.joinChannel(action, "developer-performance")
-	p.joinChannel(action, "bugs")
-
-	template, _ := template.New("Response").Parse(
-		`### Baller, I have added you to the following developer related channels!
-~developers - Great for general developer questions
-~developer-toolkit - Great questions about plugins or integrations
-~developer-meeting - Weekly core staff and community meeting
-~developer-performance - Great for questions about performance or load testing
-~bugs - To help investigate or report bugs
-`)
-	var message bytes.Buffer
-	template.Execute(&message, data)
-
-	post := &model.Post{
-		Message:   message.String(),
-		ChannelId: data.DirectMessage.Id,
-		UserId:    p.welcomeBotUserID,
-	}
-
-	if _, err := p.API.CreatePost(post); err != nil {
-		p.API.LogError(
-			"We could not create the response post",
-			"user_id", post.UserId,
-			"err", err.Error(),
-		)
-	}
-}
-
-func (p *Plugin) handleDoNotKnow(action *Action, data *MessageTemplate) {
-	p.joinChannel(action, "peer-to-peer-help")
-	p.joinChannel(action, "feature-ideas")
-	p.joinChannel(action, "developers")
-	p.joinChannel(action, "bugs")
-
-	template, _ := template.New("Response").Parse(
-		`### Great, I have added you to a few channels that might be interesting!
-~peer-to-peer-help - General help and setup
-~feature-ideas - To discuss potential feature ideas
-~developers - Great for general developer questions
-~bugs - To help investigate or report bugs
-`)
-	var message bytes.Buffer
-	template.Execute(&message, data)
-
-	post := &model.Post{
-		Message:   message.String(),
-		ChannelId: data.DirectMessage.Id,
-		UserId:    p.welcomeBotUserID,
-	}
-
-	if _, err := p.API.CreatePost(post); err != nil {
-		p.API.LogError(
-			"We could not create the response post",
-			"user_id", post.UserId,
-			"err", err.Error(),
-		)
-	}
-}
-
-func encodeEphermalMessage(w http.ResponseWriter, message string) {
+func (p *Plugin) encodeEphermalMessage(w http.ResponseWriter, message string) {
 	w.Header().Set("Content-Type", "application/json")
-	payload := map[string]interface{}{
-		"ephemeral_text": message,
+
+	resp := model.PostActionIntegrationResponse{
+		EphemeralText: message,
 	}
 
-	json.NewEncoder(w).Encode(payload)
+	if _, err := w.Write([]byte(resp.ToJson())); err != nil {
+		p.API.LogWarn("failed to write PostActionIntegrationResponse")
+	}
 }
