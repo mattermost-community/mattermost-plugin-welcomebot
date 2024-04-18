1f9919b6ff9cadaa3405f7ac22c9a3a9ac0606a3
ayusht2810 <ayush.thakur@brightscout.com>
2024-04-18 15:10:28 +0530

    Fix issue of not able to add a welcome message in private channel

--- server/command.go
+++ server/command.go
@@ -130,7 +130,7 @@ func (p *Plugin) executeCommandSetWelcome(args *model.CommandArgs) {
 		return
 	}
 
-	if channelInfo.Type == model.ChannelTypePrivate {
+	if channelInfo.Type == model.ChannelTypeDirect {
 		p.postCommandResponse(args, "welcome messages are not supported for direct channels")
 		return
 	}
